0de518764828e5ea5a40cba4039d7b13770a0cf1
Akshay898989 <akshay.gupta8989@gmail.com>
2024-06-27 17:41:33 +0530

    Initial Commit

---
+++ SSSurvey2/SSSurvey2.h
@@ -0,0 +1,18 @@
+//
+//  SSSurvey2.h
+//  SSSurvey2
+//
+//  Created by akshaygupta on 27/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSSurvey2.
+FOUNDATION_EXPORT double SSSurvey2VersionNumber;
+
+//! Project version string for SSSurvey2.
+FOUNDATION_EXPORT const unsigned char SSSurvey2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSSurvey2/PublicHeader.h>
+
+
